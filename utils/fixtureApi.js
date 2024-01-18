b4229068985f4851f1746b5654a6a338a9e96d36
ant0d0v <gosu1203@gmail.com>
2024-01-18 15:48:25 +0200

    delete class Bots Protections

--- utils/fixtureApi.js
+++ utils/fixtureApi.js
@@ -1,13 +1,9 @@
 const base = require("@playwright/test");
-import BotsProtection from "../api/BotsProtection"
 import SearchBuilder from "../api/Search.Builder"
 import SearchRequest from "../api/Search.request"
 import SearchResponse from "../api/Search.response"
 
 exports.test = base.test.extend({
-  botsProtection: async ({ request }, use) => {
-    await use(new BotsProtection(request));
-  },
   searchRequest: async ({ request }, use) => {
     await use(new SearchRequest(request));
   },
