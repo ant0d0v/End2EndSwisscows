94e08899cdab3b9a5b4a5979384939350839d3f2
ant0d0v <gosu1203@gmail.com>
2024-03-21 18:58:57 +0200

    refractors filters tests

--- tests/(search)/web/webFilters.chrome.js
+++ tests/(search)/web/webFilters.chrome.js
@@ -4,34 +4,58 @@ const { expect } = require("@playwright/test");
 const filterData = JSON.parse(
   JSON.stringify(require("../../../data/filters/testData.json"))
 );
+function getNumberYesterdayDay() {
+  const yesterday = new Date();
+  const data = yesterday.getDate() - 1;
+  return data.toString()
+}
 
+function getNumberPastMonth() {
+  const pastMonth = new Date();
+  const data = (pastMonth.getMonth());
+  return data.toString()
+}
+
+function getNumberPastYear() {
+  const numberPastYear = new Date();
+  const data = (numberPastYear.getFullYear() - 1);
+  return data.toString()
+}
 for (const {testID, expectedWebLink, locatorId, filter} of filterData.byDate) {
-  test(`${testID} ${locatorId} filter navigates to the corresponding page.`, async ({
-    app
-  }) => {
+  test(`${testID} ${locatorId} filter navigates to the corresponding page.`, async ({ app }) => {
     // Actions
-    await app.home.open()
+    await app.home.open();
     await app.home.header.clickHamburgerMenuButton();
     await app.home.header.hamburgerMenu.selectRegion("Germany");
-    await app.home.header.searchForm.inputSearchCriteria("ronaldo");
+    await app.home.header.searchForm.inputSearchCriteria("news");
     await app.home.header.searchForm.clickEnterSearchField();
-    await app.webPage.item.expectWebItemsToBeVisible()
-    await app.webPage.header.clickFiltersButton()
-    await app.webPage.filters.buttonMenu.clickFilterByDate()
-    const response = await app.webPage.filters.clickFilterInDropdownListAndGetResponse(locatorId, "/v4/web/search?query=ronaldo")
-    
-    // Assert
+    await app.webPage.item.expectWebItemsToBeVisible();
+    await app.webPage.header.clickFiltersButton();
+    await app.webPage.filters.buttonMenu.clickFilterByDate();
+
+    const response = await app.webPage.filters.clickFilterInDropdownListAndGetResponse(locatorId, "/v4/web/search?query=news");
+
+    const jsonResponse = await response.json();
+    const expectedDate = getNumberYesterdayDay()
+    const expectedDate1 = getNumberPastMonth()
+    const expectedDate2 = getNumberPastYear()
     await app.webPage.expectHaveUrl(app.page, expectedWebLink);
-    await expect(response.json()).resolves.toMatchObject({
-      request: expect.objectContaining({
-        query: "ronaldo",
-        itemsCount: 10,
-        locale: "de-DE",
-        freshness: filter,
-        includeAds: true,
-        spellcheck: true,
-        referer: expect.stringContaining(`freshness=${filter}`)
-      }),
+    jsonResponse.items.forEach(item => {
+      if (item.datePublished && testID == 1) {
+        const datePublished = item.datePublished;
+        const dateOnly = datePublished.slice(8, 10);
+        expect(dateOnly).toEqual(expectedDate);
+      }
+      if (item.datePublished && testID == 2) {
+        const datePublished = item.datePublished;
+        const dateOnly = datePublished.slice(6, 7);
+        expect(dateOnly).toEqual(expectedDate1);
+      }
+      if (item.datePublished && testID == 3) {
+        const datePublished = item.datePublished;
+        const dateOnly = datePublished.slice(0, 4);
+        expect(dateOnly).toEqual(expectedDate2);
+      }
     });
   });
 }
