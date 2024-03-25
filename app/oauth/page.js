2f32ecbc77ba40d3ef9d8b3ab3db0706caaf3fa5
ant0d0v <gosu1203@gmail.com>
2024-03-25 13:26:47 +0200

    refractor POM

---
+++ app/oauth/page.js
@@ -0,0 +1,9 @@
+import BasePage from "../../../base/BasePage.js";
+const { expect, context } = require("@playwright/test");
+
+export default class SignInPage extends BasePage {
+  constructor(page) {
+    super(page);
+    
+  }
+}
