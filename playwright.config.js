1c3300b54682f80b859aa0b934ef7666ad6a719c
ant0d0v <gosu1203@gmail.com>
2024-03-27 22:09:37 +0200

    change config file

--- playwright.config.js
+++ playwright.config.js
@@ -59,7 +59,7 @@ module.exports = defineConfig({
     trace: "on-first-retry",
   },
   expect: {
-    toHaveScreenshot: {  maxDiffPixels: 100 },
+    toHaveScreenshot: {  maxDiffPixels: 150 },
     timeout: 15 * 1000,
   },
 
@@ -74,7 +74,7 @@ module.exports = defineConfig({
       testMatch: /.*\.chrome\.js/,
       use: {
         ...devices["Desktop Chrome"],
-        // channel: "chrome",
+        channel: "chrome",
         storageState: "./data/auth/internalUser.json",
         viewport: { width: 1360, height: 900 },
         screenshot: "only-on-failure",
@@ -106,7 +106,7 @@ module.exports = defineConfig({
       testMatch: /.*\.mobile\.js/,
       use: {
         ...devices["Pixel 7"],
-        // channel: "chrome",
+        channel: "chrome",
         screenshot: "only-on-failure",
         video: "retain-on-failure",
       },
