e8631d06df0b790261e0e1c7a7e6a1c2d1afd391
ant0d0v <gosu1203@gmail.com>
2024-05-07 13:23:55 +0300

    change count  workers

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   // reporter: process.env.CI ? [['github'], ['blob']] : 'list',
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
