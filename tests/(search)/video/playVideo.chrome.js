624c4f5204cea74ee2c83e34ed5c259e61939992
ant0d0v <gosu1203@gmail.com>
2024-09-10 11:41:25 +0300

    change tests

--- tests/(search)/video/playVideo.chrome.js
+++ tests/(search)/video/playVideo.chrome.js
@@ -8,7 +8,9 @@ test("Check play video in player", async ({ app, page }) => {
   await app.home.header.searchForm.clickEnterSearchField();
   await app.videoPage.header.navigation.clickVideoTab();
   await app.videoPage.item.expectVideoItemsToBeVisible();
+  await app.page.waitForTimout(5000)
   await app.videoPage.item.clickVideoImageAt({ number: 1 });
+  await app.page.waitForTimout(5000);
   await app.videoPage.player.clickOkButton();
 
   //Assert
