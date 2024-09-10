a9558f926c3b72bbb7d38a101f721ff261c86bec
ant0d0v <gosu1203@gmail.com>
2024-09-10 12:51:12 +0300

    change tests

--- tests/(search)/video/playVideo.chrome.js
+++ tests/(search)/video/playVideo.chrome.js
@@ -13,7 +13,7 @@ test("Check play video in player", async ({ app, page }) => {
   });
   //Actions
   await app.home.open();
-  await app.home.header.searchForm.inputSearchCriteria("Skofka");
+  await app.home.header.searchForm.inputSearchCriteria("football");
   await app.home.header.searchForm.clickEnterSearchField();
   await app.videoPage.header.navigation.clickVideoTab();
   await app.videoPage.item.expectVideoItemsToBeVisible();
@@ -25,6 +25,15 @@ test("Check play video in player", async ({ app, page }) => {
 });
 
 test("Check checkbox `Don't remind me again`", async ({ app }) => {
+   await app.page.setExtraHTTPHeaders({
+     "user-agent":
+       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+     "upgrade-insecure-requests": "1",
+     accept:
+       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+     "accept-encoding": "gzip, deflate, br",
+     "accept-language": "en-US,en;q=0.9,en;q=0.8",
+   });
   //Actions
   await app.home.open();
   await app.home.header.searchForm.inputSearchCriteria("football");
