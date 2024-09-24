64700c17d8a0c7a8971b7ad73f2d9f322093e6d5
ant0d0v <gosu1203@gmail.com>
2024-09-24 20:56:45 +0300

    refactor Check content of education  page for  localization, Check content of imprint page for  localization

--- app/(pages)/imprint/page.js
+++ app/(pages)/imprint/page.js
@@ -1,14 +1,19 @@
 import BasePage from "../../../base/BasePage.js";
 import Header from "../../(pages)/Header.js";
+import Translations from "../../../locales/n18next.js";
 export default class ImprintPage extends BasePage {
   constructor(page) {
     super(page);
+    this.translations = Translations;
     this.header = new Header(page);
 
     //Locators
-    this.allContent = this.page.locator("main.imprint");
-    this.allImages = this.page.locator("main.imprint img:visible");
-    this.companyInfoImage = this.page.locator(".company-info img");
+    this.root = this.page.locator(".imprint");
+    this.title = this.root.locator("h1");
+    this.images = this.root.locator("img:visible");
+    this.companyInfoImage = this.root.locator(".company-info img");
+    this.locationInfo = this.root.locator(".location");
+    this.publishingInfo = this.root.locator(".publishing-info p");
   }
   //Actions
   async open() {
@@ -17,9 +22,61 @@ export default class ImprintPage extends BasePage {
 
   //Verify
   takeSnapshot = async (testInfo) => {
-    await this.expectPageToHaveScreenshotWithoutMask(testInfo, this.allImages);
+    await this.expectPageToHaveScreenshotWithoutMask(testInfo, this.images);
   };
   expectCompanyInfoImageToBeVisible = async () => {
     await this.expectElementToBeVisible(this.companyInfoImage);
   };
+
+  //Locales
+  async expectTranslationsForPublishingInfo(
+    expected = {
+      translationKey_1: value,
+      translationKey_2: value,
+      translationKey_3: value,
+      translationKey_4: value,
+      translationKey_5: value,
+      translationKey_6: value,
+      locale: value,
+    }
+  ) {
+    const expectedTitle = this.translations.t(expected.translationKey_1, {
+      lng: expected.locale,
+    });
+    const expectedDescription_1 = this.translations.t(expected.translationKey_2, {
+      lng: expected.locale,
+    });
+    const expectedDescription_2 = this.translations.t(expected.translationKey_3, {
+      lng: expected.locale,
+    });
+    const expectedDescription_3 = this.translations.t(expected.translationKey_4, {
+      lng: expected.locale,
+    });
+    const expectedDescription_4 = this.translations.t(expected.translationKey_5, {
+      lng: expected.locale,
+    });
+    const expectedDescription_5 = this.translations.t(expected.translationKey_6, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.publishingInfo, [
+      expectedTitle,
+      expectedDescription_1,
+      expectedDescription_2,
+      expectedDescription_3,
+      expectedDescription_4,
+      expectedDescription_5,
+    ]);
+  }
+  async expectTranslationsForLocation (expected = { translationKey: value, locale: value }) {
+    const expectedLocation = this.translations.t(expected.translationKey, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.locationInfo, expectedLocation);
+  }
+  async expectTranslationsForTitle (expected = { translationKey: value, locale: value }) {
+    const expectedTitle = this.translations.t(expected.translationKey, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.title, expectedTitle);
+  }
 }
--- app/(pages)/media-education/page.js
+++ app/(pages)/media-education/page.js
@@ -1,6 +1,7 @@
 import BasePage from "../../../base/BasePage.js";
 import videoPlayer from "../../../components/VideoPlayer.js";
 import Header from "../../(pages)/Header.js";
+import Translations from "../../../locales/n18next.js";
 import  { expect, test } from"@playwright/test";
 
 export default class MediaEducationPage extends BasePage {
@@ -8,17 +9,20 @@ export default class MediaEducationPage extends BasePage {
     super(page);
     this.videoPlayer = new videoPlayer(page);
     this.header = new Header(page);
+    this.translations = Translations;
 
     //Locators
-    this.allContent = this.page.locator("main.media-education");
-    this.allImages = this.page.locator("main.media-education img:visible");
+    this.root = this.page.locator(".media-education");
+    this.title = this.root.locator("h1");
+    this.blockTitle = this.root.locator("h2");
+    this.descriptions = this.root.locator("p");
+    this.images = this.root.locator("img:visible");
     this.pdfLinks = (id) => this.page.getByRole("link", { name: `${id}` });
     this.links = (id) =>
       this.page.getByRole("link", { name: `${id}`, exact: true });
-    this.player = this.page.locator("div.player");
-    this.flyerButton = this.page.getByRole("link", {
-      name: "Open flyer [PDF, 1.49Mb]",
-    });
+    this.player = this.page.locator(".player");
+    this.flyerButton = this.root.locator(".button");
+    this.mediaeduList = this.root.locator(".media-edu-list");
   }
 
   //Actions
@@ -49,15 +53,13 @@ export default class MediaEducationPage extends BasePage {
   //Assert
 
   takeSnapshot = async (testInfo) => {
-    await this.expectPageToHaveScreenshot(
-      testInfo,
-      this.allImages,
-      this.player
-    );
+    await this.expectPageToHaveScreenshot(testInfo, this.images, this.player);
   };
   expectColorWhenHoveringOnPdfButton = async (expectedValue) => {
     await this.expectColorLinkWhenHovering(
-      this.flyerButton,"background", expectedValue
+      this.flyerButton,
+      "background",
+      expectedValue
     );
   };
 
@@ -80,5 +82,71 @@ export default class MediaEducationPage extends BasePage {
         maxDiffPixelRatio: 0.5,
       });
     });
+  }
+  //Locales
+  async expectTranslationsForDescriptions(
+    expected = {
+      translationKey_1: value,
+      translationKey_2: value,
+      translationKey_3: value,
+      translationKey_4: value,
+      translationKey_5: value,
+      translationKey_6: value,
+      translationKey_7: value,
+      translationKey_8: value,
+      translationKey_9: value,
+      translationKey_10: value,
+      translationKey_11: value,
+      translationKey_12: value,
+      translationKey_13: value,
+      translationKey_14: value,
+      locale: value,
+    }
+  ) {
+    const list = [];
+    for (let i = 1; i <= 14; i++) {
+      const key = `translationKey_${i}`;
+      const translation = this.translations.t(expected[key], {
+        lng: expected.locale,
+      });
+      if (translation) {
+        list.push(translation);
+      }
+    }
+    await this.expectElementToHaveText(this.descriptions, list);
+  }
+  async expectTranslationsForBlockTitle(
+    expected = {
+      translationKey_1: value,
+      translationKey_2: value,
+      locale: value,
+    }
+  ) {
+    const expectedTitle_1 = this.translations.t(expected.translationKey_1, {
+      lng: expected.locale,
+    });
+    const expectedTitle_2 = this.translations.t(expected.translationKey_2, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.blockTitle, [
+      expectedTitle_1,
+      expectedTitle_2,
+    ]);
+  }
+  async expectTranslationsForFlayerButton(
+    expected = { translationKey: value, locale: value }
+  ) {
+    const expectedButtonText = this.translations.t(expected.translationKey, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.flyerButton, expectedButtonText);
+  }
+  async expectTranslationsForMediaeduList(
+    expected = { translationKey: value, locale: value }
+  ) {
+    const expectedText = this.translations.t(expected.translationKey, {
+      lng: expected.locale,
+    });
+    await this.expectElementToHaveText(this.mediaeduList, expectedText);
   }
 }
