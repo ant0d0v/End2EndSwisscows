f05e3faff02c40d52591033280c54f0aa326cbde
ant0d0v <gosu1203@gmail.com>
2024-11-18 00:20:12 +0200

    refractor function name route

--- app/api/route.js
+++ app/api/route.js
@@ -15,7 +15,7 @@ export default class Route extends PageHolder {
       }
     );
   };
-  
+
   requestWithGivenResponse = async (endpoint, body) => {
     await this.page.route(process.env.API_URL + `${endpoint}/**`, (route) =>
       route.fulfill({
